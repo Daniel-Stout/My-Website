60e648b26a62b9edcaf37617469e3ffbe5d48e47
Daniel-Stout <45444420+Daniel-Stout@users.noreply.github.com>
2019-07-14 11:31:37 -0500

    Delete lolwut.ps1

--- lolwut.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.speech
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak(\"Here's a joke. Why do Java programmers wear glasses? Because they can't C#.\")
-exit
